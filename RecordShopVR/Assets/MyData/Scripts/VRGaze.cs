f136d2b8463615ac1a38cf0ebd2c9dfdc235e12a
Max-Swatek <max@taiv.tv>
2019-03-10 17:28:13 -0500

    first
    Skeleton Code to start with, will become great soon

---
+++ RecordShopVR/Assets/MyData/Scripts/VRGaze.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VRGaze : MonoBehaviour
+{
+    public Image imgGaze;
+    public float totalTime = 2;
+    bool gvrStatus;
+    float gvrTimer;
+
+    public int distanceOfRay = 10;
+    private RaycastHit _hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gvrStatus)
+        {
+            gvrTimer += Time.deltaTime;
+            imgGaze.fillAmount = gvrTimer / totalTime;
+        }
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        if(Physics.Raycast(ray, out _hit, distanceOfRay))
+        {
+            if((int)imgGaze.fillAmount == 1 && _hit.transform.CompareTag("Teleport"))
+            {
+                _hit.transform.gameObject.GetComponent<Teleport>().TeleportPlayer();
+            }
+
+            if ((int)imgGaze.fillAmount == 1 && _hit.transform.CompareTag("RotateBox"))
+            {
+                _hit.transform.gameObject.GetComponent<RotateBox>().ChangeSpin();
+            }
+        }
+    }
+
+    public void gvrOn()
+    {
+        gvrStatus = true;
+    }
+
+    public void gvrOff()
+    {
+        gvrStatus = false;
+        gvrTimer = 0;
+        imgGaze.fillAmount = 0;
+    }
+
+}
